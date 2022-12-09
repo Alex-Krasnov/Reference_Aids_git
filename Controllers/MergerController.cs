ca7fb40339e450ffcbd975185e1d5f386de52471
Alex Krasnov <96766826+Alex-Krasnov@users.noreply.github.com>
2022-12-09 11:06:07 +0300

    добавлено превью на объединение

--- Controllers/MergerController.cs
+++ Controllers/MergerController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.FlowAnalysis;
 using Microsoft.EntityFrameworkCore;
 using Reference_Aids.Data;
 using Reference_Aids.Models;
+using Reference_Aids.ViewModels;
 
 namespace Reference_Aids.Controllers
 {
@@ -19,30 +21,64 @@ namespace Reference_Aids.Controllers
             return View("Index");
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Merger(int PatientId1, int PatientId2)
+        
+        public async Task<IActionResult> Preview(int PatientId1, int PatientId2)
         {
-            List<TblDistrictBlot> districtBlots = _context.TblDistrictBlots.Where(e => e.PatientId == PatientId2).ToList();
+            var patients = new List<PatientFull>
+            {
+                new PatientFull()
+                {
+                    TblPatientCards = await _context.TblPatientCards.Where(e => e.PatientId == PatientId1).ToListAsync(),
+                    TblDistrictBlots = await _context.TblDistrictBlots.Where(e => e.PatientId == PatientId1).ToListAsync(),
+                    TblIncomingBloods = await _context.TblIncomingBloods.Where(e => e.PatientId == PatientId1).ToListAsync()
+                },
+                new PatientFull()
+                {
+                    TblPatientCards = await _context.TblPatientCards.Where(e => e.PatientId == PatientId2).ToListAsync(),
+                    TblDistrictBlots = await _context.TblDistrictBlots.Where(e => e.PatientId == PatientId2).ToListAsync(),
+                    TblIncomingBloods = await _context.TblIncomingBloods.Where(e => e.PatientId == PatientId2).ToListAsync()
+                }
+            };
+
+            var Viewdata = new ListForMergerViewModel
+            {
+                ListSexes = await _context.ListSexes.ToListAsync(),
+                ListRegions = await _context.ListRegions.ToListAsync(),
+                ListSendLabs = await _context.ListSendLabs.ToListAsync(),
+                ListTestSystems = await _context.ListTestSystems.ToListAsync(),
+                Patients = patients,
+                ListSendDistricts = await _context.ListSendDistricts.ToListAsync(),
+                ListCategories = await _context.ListCategories.OrderBy(e => e.CategoryId).ToListAsync(),
+                ListQualitySerums = await _context.ListQualitySerums.ToListAsync()
+            };
+            ViewBag.Title = "MergerPatientPreview";
+            return View("Preview", Viewdata);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Merger(int PatientId_1, int PatientId_2)
+        {
+            List<TblDistrictBlot> districtBlots = _context.TblDistrictBlots.Where(e => e.PatientId == PatientId_2).ToList();
             if (districtBlots != null)
             {
                 foreach (var b in districtBlots)
                 {
-                    b.PatientId = PatientId1;
+                    b.PatientId = PatientId_1;
                     await _context.SaveChangesAsync();
                 }
             }
 
-            List<TblIncomingBlood> incomingBloods = _context.TblIncomingBloods.Where(e => e.PatientId == PatientId2).ToList();
+            List<TblIncomingBlood> incomingBloods = _context.TblIncomingBloods.Where(e => e.PatientId == PatientId_2).ToList();
             if (incomingBloods != null)
             {
                 foreach (var i in incomingBloods)
                 {
-                    i.PatientId = PatientId1;
+                    i.PatientId = PatientId_1;
                     await _context.SaveChangesAsync();
                 }
             }
 
-            var patient = _context.TblPatientCards.First(e => e.PatientId == PatientId2);
+            var patient = _context.TblPatientCards.First(e => e.PatientId == PatientId_2);
             _context.TblPatientCards.Remove(patient);
             await _context.SaveChangesAsync();
 
---
+++ ViewModels/ListForMergerViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Reference_Aids.Models;
+
+namespace Reference_Aids.ViewModels
+{
+    public class ListForMergerViewModel
+    {
+        public IEnumerable<ListSex> ListSexes { get; set; }
+        public IEnumerable<ListRegion> ListRegions { get; set; }
+        public IEnumerable<ListSendDistrict> ListSendDistricts { get; set; }
+        public IEnumerable<ListSendLab> ListSendLabs { get; set; }
+        public IEnumerable<ListTestSystem> ListTestSystems { get; set; }
+        public IEnumerable<ListCategory> ListCategories { get; set; }
+        public IEnumerable<ListQualitySerum> ListQualitySerums { get; set; }
+
+        public List<PatientFull> Patients { get; set; }
+        
+        
+    }
+    public class PatientFull
+    {
+        public List<TblPatientCard> TblPatientCards { get; set; }
+        public IEnumerable<TblDistrictBlot> TblDistrictBlots { get; set; }
+        public IEnumerable<TblIncomingBlood> TblIncomingBloods { get; set; }
+    }
+}
